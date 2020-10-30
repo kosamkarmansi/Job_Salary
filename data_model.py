0912b3aa87296a8322b61fab3d0ad44e7077b057
Mansi Kosamkar <kosamkarmansi@gmail.com>
2020-10-30 17:07:03 -0400

    added data model script

---
+++ data_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 21:58:40 2020
+
+@author: mansikosamkar
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('eda_data.csv')
+
+#relevant columns
+df.columns  
